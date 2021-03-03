985ce072722d445e0b35d602648e6001a4d48d19
breakerbay <breakerbay@users.noreply.github.com>
2021-03-04 12:26:17 +1300

    Added images.svelte

---
+++ src/components/Images.svelte
@@ -0,0 +1,15 @@
+<script>
+    const fetchImage = (async () => {
+        const response = await fetch('http://brogo.net.au/checklist/api/gallery')
+        return await response.json()
+    })()
+</script>
+
+{#await fetchImage}
+    <p>...waiting</p>
+{:then data}
+<!--    <img src={data.message} alt="Dog image" />-->
+    <h3>To Do</h3>
+{:catch error}
+    <p>An error occurred!</p>
+{/await}
