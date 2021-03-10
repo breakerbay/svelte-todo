7c929d9c9904d783a00802f4a16ce5b5f948268b
breakerbay <breakerbay@users.noreply.github.com>
2021-03-11 10:47:36 +1300

    Update CheckerProjectSummary.svelte

--- src/components/checker/CheckerProjectSummary.svelte
+++ src/components/checker/CheckerProjectSummary.svelte
@@ -1,8 +1,39 @@
 <script>
-    export let projectSummary;
+    export let project;
 </script>
 
-<!-- Project Summary -->
+<div class="card">
+<!--    <img src="img_avatar.png" alt="Avatar" style="width:100%">-->
+    <div class="container">
+        <h4><b>{project.name}</b></h4>
+        <p>{project.id}</p>
+        <p>{project.accountId}</p>
+        <p>{project.number}</p>
+        <p>{project.accountName}</p>
+        <p>{project.accountUserId}</p>
+        <p>{project.accountOwner}</p>
+    </div>
+</div>
+
+<style>
+.card {
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+/* Add some padding inside the card container */
+.container {
+  padding: 2px 16px;
+}
+</style>
+
+<!--
 Block links
 Object fit
 Address
@@ -18,15 +49,6 @@ https://www.w3schools.com/howto/howto_css_coupon.asp
 https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_coupon
 https://github.com/mdn/css-examples
 
-
-<!--
-"id": "389",
-"accountId": "89",
-"number": "1",
-"name": "Feature List",
-"accountName": "Checklist Engine Web Site",
-"accountUserId": "22",
-"accountOwner": "Richard Hancock"
-
 TODO Add Address, Site Photo, Street Map to location
 -->
+
