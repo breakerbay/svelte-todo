ce53848e193770c75e8c3d808fede907ea6625eb
Richard Hancock <breakerbay@hotmail.com>
2021-03-18 14:09:55 +1300

    Added url query parameter criteria=true

--- src/components/checker/Checklists.svelte
+++ src/components/checker/Checklists.svelte
@@ -9,7 +9,7 @@
         console.log("CheckerProject, getProject, id: " + id);
         loading = true;
         try {
-            response = await fetch("http://localhost/~richardhancock/checklist/api/checkers/22/projects/" + id + "?details=all");
+            response = await fetch("http://localhost/~richardhancock/checklist/api/checkers/22/projects/" + id + "?details=all&criteria=true");
         } catch (e) {
             console.error(`CheckerProject, error: ${e}`);
             // response = await fetch("/static/api/projects/389/project-389.json");
